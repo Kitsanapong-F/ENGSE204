fabf517ff23cf181529fa8da61cf169c5d763abd
Kitsanapong-F <09chaisu@gmail.com>
2025-11-20 14:59:05 +0700

    ENGSE204

--- lab1.java
+++ lab1.java
@@ -1,12 +1,10 @@
-import java.util.Scanner;
+import java.util.Scanner; //Import
 public class lab1 {
     public static void main(String[] args) {
-       
-        Scanner input = new Scanner(System.in);
-        int numBer1 = input.nextInt();
-        int numBer2 = input.nextInt();
-        System.out.println(numBer1+numBer2);
-        
-        input.close();
+        Scanner input = new Scanner(System.in);  //สร้าง Scanner
+        int numBer1 = input.nextInt();   //รับข้อมูล (numBer1)
+        int numBer2 = input.nextInt();   //รับข้อมูล (numBer2)
+        System.out.println(numBer1+numBer2); //แสดงผล
+        input.close(); //ปิดScanner
     }
 }

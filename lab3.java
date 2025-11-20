dacfa1ee87bed5a80e58b2432009c9cfb6d1c7d4
Kitsanapong-F <09chaisu@gmail.com>
2025-11-20 21:11:19 +0700

    ENGSE204

--- lab3.java
+++ lab3.java
@@ -1,19 +1,19 @@
-import java.util.Scanner;
+import java.util.Scanner;//import Scanner
 
 public class lab3 {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int number = input.nextInt();
-
+        Scanner input = new Scanner(System.in);// สร้างScanner
+        int number = input.nextInt();//รับค่า number
         String num = add(number);
-        System.out.println(num);
+        System.out.println(num);//แสดง num
+        input.close();// ปิดScanner
     }
     public static String add(int a) {
-        String A;
+        String A; //กำหนดค่า A
         if(a % 2 == 0){
-            A = "Even";
+            A = "Even";//กำหนดค่า A
         } else {
-            A = "Odd";
+            A = "Odd";//กำหนดค่า A
         }
         return A;
     }

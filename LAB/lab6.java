86eadd9b8e425036cfa6df171e589f33f6ed73d0
Kitsanapong-F <09chaisu@gmail.com>
2025-11-22 11:07:41 +0700

    ENGSE204

--- LAB/lab6.java
+++ LAB/lab6.java
@@ -1,13 +1,13 @@
 package LAB;
-import java.util.Scanner;
+import java.util.Scanner; // import Scanner
 public class lab6 {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);
-        int numBer = input.nextInt();
+        Scanner input = new Scanner(System.in); // สร้างScanner
+        int numBer = input.nextInt(); 
         for(int i = 1 ;i<=12;i++){
-            System.out.printf("%d x %d = %d\n",numBer,i,(numBer*i));
+            System.out.printf("%d x %d = %d\n",numBer,i,(numBer*i));  //แสดงค่าแม่สูตรคูณ
         }
-        input.close();
+        input.close(); // ปิดScanner
     }
 }
 

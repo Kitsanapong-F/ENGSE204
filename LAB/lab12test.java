6ab9fbf3dce71b377b2f93d1ee65120d9cf8534e
Kitsanapong-F <09chaisu@gmail.com>
2025-11-22 22:38:52 +0700

    ENGSE

---
+++ LAB/lab12test.java
@@ -0,0 +1,21 @@
+package LAB;
+import java.util.Scanner;
+public class lab12test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int numBer = input.nextInt();
+        int[][] x = readArRay(input, numBer);
+        
+
+        input.close();
+    }
+    public static int[][] readArRay(Scanner input,int a ){
+        int[] proDuctID = new int[a];
+        int[] stockQuantiTy = new int[a];
+        for(int i = 0;i<proDuctID.length;i++){
+            proDuctID[i] = input.nextInt();
+            stockQuantiTy[i] = input.nextInt(); 
+        }
+        return new int[][]{proDuctID,stockQuantiTy};
+    }
+}
--- LAB/lab2.java
+++ LAB/lab2.java
@@ -1,11 +1,17 @@
 package LAB;
-import java.util.Scanner; // importScanner
+import java.util.Scanner; 
 public class lab2 {
     public static void main(String[] args){
-        Scanner input = new Scanner(System.in);//สร้างscanner
-        double width = input.nextDouble();//รับค่า width
-        double height = input.nextDouble();//รับค่า height
-        System.out.println(width*height); // แสดงผล width*height
-        input.close();//ปิดScanner
+        Scanner input = new Scanner(System.in);
+        double result = area(input);
+        System.out.printf("%.2f",result);
+        input.close();
+    }
+    public static double area(Scanner input){
+        double sum = 0;
+        double width = input.nextInt();
+        double height = input.nextInt();
+        sum= width*height;
+        return sum;
     }
 }

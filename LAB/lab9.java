572905292a2e0a2e751e383b5a5c2f1e8b50dc9a
Kitsanapong-F <09chaisu@gmail.com>
2025-11-22 15:04:33 +0700

    ENGSE204

--- LAB/lab9.java
+++ LAB/lab9.java
@@ -3,15 +3,25 @@ import java.util.Scanner;
 public class lab9 {
     public static void main(String[] AverAge) {
         Scanner input = new Scanner(System.in);
-        int x = input.nextInt();
-        double sum=0;
-        int[] numBers = new int[x];
-        for(int i = 0;i<numBers.length;i++){
-            numBers[i] = input.nextInt();
-            sum = sum + numBers[i];
-        }
-        double averAge = (sum/x);
-        System.out.print(averAge);
+        int[] x = read(input);
+        double area = calculate(x);
+        System.out.printf("%.2f",area);
         input.close();
+      
+    }
+    public static int[] read(Scanner input){
+        int numBer = input.nextInt();
+        int num[] = new int[numBer];
+        for(int i = 0;i<num.length;i++){
+            num[i] = input.nextInt();
+        }
+        return num;
+    }
+    public static double calculate(int[] math){
+        double sum=0;
+        for(int j = 0;j<math.length;j++){
+            sum = sum+math[j];
+        }
+        return sum/math.length;
     }
 }

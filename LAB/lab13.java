a9e9a9a2595fe63aeea3c778b7cf8e5eb041b9d5
Kitsanapong-F <09chaisu@gmail.com>
2025-11-23 11:20:48 +0700

    ENFSE204

--- LAB/lab13.java
+++ LAB/lab13.java
@@ -1,4 +1,26 @@
 package LAB;
+import java.util.Scanner;;
 public class lab13 {
-    
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int R = input.nextInt();
+        int C = input.nextInt();
+        int[][]  arRay2D = new int[R][C];
+        for(int i = 0;i<R;i++){
+            for(int j = 0;j<C;j++){
+                arRay2D[i][j] = input.nextInt();
+            }
+        }
+        int onLine=0;
+         for(int i = 0;i<R;i++){
+            for(int j = 0;j<C;j++){
+                if(arRay2D[i][j]==1){
+                    onLine=onLine+1;
+                }
+            }
+        }
+        System.out.printf("%d",onLine);
+        
+        input.close();
+    }
 }
---
+++ lab13test.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class lab13test {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int rows = input.nextInt(); 
+        int cols = input.nextInt(); 
+        int[][] arr = new int[rows][cols];
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                arr[i][j] = input.nextInt();
+            }
+        }
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < cols; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+        input.close();
+    }
+}
+

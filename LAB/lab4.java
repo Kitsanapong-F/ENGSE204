a0b405e5e83d8d3ddfec0a55dc8263b98994e09f
Kitsanapong-F <09chaisu@gmail.com>
2025-11-23 19:07:50 +0700

    ENFSE204

--- LAB/lab4.java
+++ LAB/lab4.java
@@ -1,27 +1,25 @@
 package LAB;
 import java.util.Scanner;
+
 public class lab4 {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int numBer = input.nextInt();
-        if( numBer>=80 && numBer<=100){
+
+        if (numBer >= 80 && numBer <= 100) {
             System.out.println("A");
-        }
-        else if( numBer>=70){
+        } else if (numBer >= 70 && numBer <= 79) {
             System.out.println("B");
-        }
-         else if( numBer>=60 ){
+        } else if (numBer >= 60 && numBer <= 69) {
             System.out.println("C");
-        }
-         else if( numBer>=50 ){
+        } else if (numBer >= 50 && numBer <= 59) {
             System.out.println("D");
-        }
-         else if( numBer>=0 ){
+        } else if (numBer >= 0 && numBer <= 49) {
             System.out.println("F");
-        }
-        else{
+        } else {
             System.out.println("enternumber");
         }
+
         input.close();
     }
 }
